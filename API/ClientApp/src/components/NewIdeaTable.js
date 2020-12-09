b430f9261613f5f2cbca7ef4524d4017f5734eeb
Ram Rai <ram_maan2@yahoo.com>
2020-12-09 12:51:42 +0100

    fetch ideas into table

--- API/ClientApp/src/components/NewIdeaTable.js
+++ API/ClientApp/src/components/NewIdeaTable.js
@@ -1,39 +1,55 @@
-import React,{Component, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Table from 'react-bootstrap/Table';
+import ideaService from '../services/IdeaService';
 
-export default class NewIdeaTable extends Component {
+const NewIdeas = (props) => {
+  const [ideas, setIdeas] = useState('');
+  useEffect(async() => {
+    const result = await ideaService.getAll();
+    setIdeas(result.data);
+  }, []);
+ 
+  const populateIdeas = () => {
+    if (ideas != '')
+    {
+      const list = [];
 
-    render () {
-         return (
-          <div>
-          <h1>New Ideas</h1>
-          <br/>
-            <Table striped bordered hover>
-          <thead class="thead-dark">
-          <tr>
-          <th>S.no</th>
-          <th>Title</th>
-          <th>Effort</th>
-          <th>Impact</th>
-          </tr>
-          </thead>
-          <tbody>
-         <tr>
-          <td>1</td>
-          <td>Time Management</td>
-          <td>2</td>
-          <td>unknown</td>
-          </tr>
-          <tr>
-          <td>2</td>
-          <td>Team work</td>
-          <td>3</td>
-          <td></td>
-          </tr>
+    const data = ideas.data;
+    console.log(data);
+    for (let i = 0; i < data.length; i++) {
+      list.push(
+        <tr>
+          <td>{data.ideaId}</td>
+          <td>{data.title}</td>
+          <td>{data.effort}</td>
+          <td>{data.impact}</td>
+        </tr>
+      );
+    }
+
+    return list;
+    }
     
+    return null;
+  }
+  return (
+    <div>
+      <h1>New Ideas</h1>
+      <br />
+      <Table striped bordered hover>
+        <thead className="thead-dark">
+          <tr>
+            <th>S.no</th>
+            <th>Title</th>
+            <th>Effort</th>
+            <th>Impact</th>
+          </tr>
+        </thead>
+        <tbody>
+         {populateIdeas()}
         </tbody>
       </Table>
-      </div>
-         )
-    }
+    </div>
+  );
 }
+export default NewIdeas;
