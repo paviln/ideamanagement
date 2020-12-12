31f02306c76a3ebe2d89c35f0a6d9195cda20ad3
Jens Christensen <paviln@outlook.dk>
2020-12-12 17:28:47 +0100

    Added comment model to idea

---
+++ API/Data/Migrations/20201212162810_CommentIdeaSchema.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EskobInnovation.IdeaManagement.API.Data.Migrations
+{
+    public partial class CommentIdeaSchema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "IdeaComment",
+                columns: table => new
+                {
+                    IdeaCommentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Text = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IdeaId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IdeaComment", x => x.IdeaCommentId);
+                    table.ForeignKey(
+                        name: "FK_IdeaComment_Ideas_IdeaId",
+                        column: x => x.IdeaId,
+                        principalTable: "Ideas",
+                        principalColumn: "IdeaId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IdeaComment_IdeaId",
+                table: "IdeaComment",
+                column: "IdeaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "IdeaComment");
+        }
+    }
+}
