3cd665fcbef408c6d39d2f2442b4586a47ff926b
Jens Christensen <paviln@outlook.dk>
2020-12-13 10:55:34 +0100

    Comments layout

--- API/ClientApp/src/components/Comments.js
+++ API/ClientApp/src/components/Comments.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ListGroup from 'react-bootstrap/ListGroup';
+import moment from 'moment';
 
 function Comments(props) {
 
@@ -9,9 +9,15 @@ function Comments(props) {
     if (comments !== null) {
       for (let i = 0; i < comments.length; i++) {
         list.push(
-          <ListGroup.Item>
-            {comments[i].text}
-          </ListGroup.Item>
+          <div className="d-flex bg-light border border-dark rounded p-2">
+            <div className="flex-grow-1">
+              <p>{comments[i].name}</p>
+              <p>{comments[i].text}</p>
+            </div>
+            <div className="align-self-center">
+              <p >{moment(comments[i].date).format("DD/MM/YYYY, HH:mm:ss")}</p>
+            </div>
+          </div>
         );
       }
     }
@@ -24,10 +30,8 @@ function Comments(props) {
   if (comments.length > 0) {
     return (
       <div>
-        <p className="pt-4">Comments</p>
-        <ListGroup>
-          {comments}
-        </ListGroup>
+        <p className="pt-4 pb-2">Comments</p>
+        {comments}
       </div>
     );
   }
