eb146f509fc6b7f2059c88b7689c317746923f36
Jens Christensen <paviln@outlook.dk>
2020-12-13 20:10:02 +0100

    Added challenge and result to Idea model

---
+++ API/Data/Migrations/20201213190924_ChallengeResultIdeaSchema.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EskobInnovation.IdeaManagement.API.Data.Migrations
+{
+    public partial class ChallengeResultIdeaSchema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Challenge",
+                table: "Ideas",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Result",
+                table: "Ideas",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Challenge",
+                table: "Ideas");
+
+            migrationBuilder.DropColumn(
+                name: "Result",
+                table: "Ideas");
+        }
+    }
+}
