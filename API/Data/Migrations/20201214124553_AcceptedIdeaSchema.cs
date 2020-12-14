de82a8772df6e4d55feb913053f0247fa61eac3f
Jens Christensen <paviln@outlook.dk>
2020-12-14 13:46:21 +0100

    Added accpeted to idea

---
+++ API/Data/Migrations/20201214124553_AcceptedIdeaSchema.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EskobInnovation.IdeaManagement.API.Data.Migrations
+{
+    public partial class AcceptedIdeaSchema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Accepted",
+                table: "Ideas",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Accepted",
+                table: "Ideas");
+        }
+    }
+}
